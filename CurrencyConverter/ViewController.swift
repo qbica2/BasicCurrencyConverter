c9ecc39f5c5fd545583460077b9cffd346712ffc
Kubilay Akdemir <kubidik27@gmail.com>
2023-01-20 14:07:08 +0300

    corner radius added to labels

--- CurrencyConverter/ViewController.swift
+++ CurrencyConverter/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var topLabel: UILabel!
+    @IBOutlet weak var bottomLabel: UILabel!
+    @IBOutlet weak var fromLabel: UILabel!
+    @IBOutlet weak var toLabel: UILabel!
+    @IBOutlet weak var amountTextField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        fromLabel.layer.cornerRadius = 8
+        toLabel.layer.cornerRadius = 8
+        resultLabel.layer.cornerRadius = 8
+        fromLabel.layer.masksToBounds = true
+        toLabel.layer.masksToBounds = true
+        resultLabel.layer.masksToBounds = true
+
     }
 
-
+    
+    
+    @IBAction func didTappedConvertButton(_ sender: Any) {
+    }
+    
 }
 
